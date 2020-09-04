65027da335ba66ea699235159bd8b859d341c93a
Radu Cristea <Radu.Cristea@endava.com>
2020-09-04 14:42:26 +0300

    converting fractions from one base to another #working #draft

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         var scanner = new Scanner(System.in);
 
@@ -11,20 +12,112 @@ public class Main {
         String stringNumber = scanner.next();
         int targetRadix = scanner.nextInt();
 
-        String nr = convert(stringNumber, sourceRadix, targetRadix);
+        int integralPart = 0;
+        double decimalPart = 0;
+        String fractional;
 
-        System.out.println(nr);
-    }
+        int pointIndex = stringNumber.indexOf('.');
 
-    private static String convert(String nr, int sourceRadix, int targetRadix) {
-        if (targetRadix == 1) {
-            char[] ones = getOnes(nr);
-            return new String(ones);
+        if (sourceRadix != 10) {
+
+            String integral;
+            if (pointIndex > 0) {
+                integral = stringNumber.substring(0, pointIndex);
+
+                fractional = stringNumber.substring(pointIndex + 1);
+                char[] fractionalDigits = fractional.toCharArray();
+
+                for (int i = 0; i < fractionalDigits.length; i++) {
+                    int nrRepresentation = Character.getNumericValue(fractionalDigits[i]);
+                    double n =  nrRepresentation / Math.pow(sourceRadix, i + 1);
+                    decimalPart += n;
+                }
+            } else {
+                integral = stringNumber;
+            }
+
+            integralPart = toDecimal(integral, sourceRadix);
+        } else {
+            if (pointIndex > 0) {
+                String integral = stringNumber.substring(0, pointIndex);
+                integralPart = Integer.parseInt(integral);
+
+                fractional = stringNumber.substring(pointIndex + 1);
+                decimalPart = Double.parseDouble("0." + fractional);
+            } else {
+                integralPart = toDecimal(stringNumber, sourceRadix);
+            }
         }
 
-        int decimal = toDecimal(nr, sourceRadix);
+        String integerNr = Integer.toString(integralPart, targetRadix);
+        if (targetRadix == 1) {
+            integerNr = String.valueOf(getOnes(integerNr));
+        }
 
-        return Integer.toString(decimal, targetRadix);
+        StringBuilder fractionalNr = new StringBuilder();
+
+        if (decimalPart > 0.0) {
+
+            double nr;
+            for (int i = 0; i < 5; i++) {
+                nr = decimalPart * targetRadix;
+
+                int integer = getIntegerPart(nr, targetRadix);
+                if (targetRadix > 10) {
+                    String convertedNr = Integer.toString(integer, targetRadix);
+
+                    int intRepresentation = Integer.parseInt(convertedNr);
+                    if (intRepresentation >= 10) {
+                        char chDigit = getCharDigit(intRepresentation);
+
+                        fractionalNr.append(chDigit);
+                    } else {
+                        fractionalNr.append(convertedNr);
+                    }
+                } else {
+                    fractionalNr.append(integer);
+                }
+
+                decimalPart = getFractionalPart(nr);
+            }
+        }
+
+        System.out.print(integerNr);
+        if (decimalPart > 0.0) {
+            System.out.print("." + fractionalNr);
+        }
+    }
+
+    private static char getCharDigit(int integer) {
+        char chA = 'a';
+
+        int digitCount = chA - 10 + integer;
+
+        return (char) digitCount;
+    }
+
+    // Get the integer part from a decimal number
+    // and converts it to the specified radix
+    private static int getIntegerPart(double nr, int radix) {
+        String stringNr = Double.toString(nr);
+        String integralValue = stringNr.substring(0, stringNr.indexOf('.'));
+
+        String stringInteger = stringNr.indexOf('.') > 0
+                               ? integralValue
+                               : stringNr;
+
+        return toDecimal(stringInteger, radix);
+    }
+
+    private static double getFractionalPart(double nr) {
+        String stringNr = Double.toString(nr);
+        String decimalValue = stringNr.substring(stringNr.indexOf('.') + 1);
+
+        String stringFractional = stringNr.indexOf('.') > 0
+                                  ? decimalValue
+                                  : "0";
+
+        return Double.parseDouble("0." + stringFractional);
     }
 
     private static int toDecimal(String nr, int sourceRadix) {
